4ee0416c5c186be39b3ae28f901eca8e8c0a526f
144234 <78003838+144234@users.noreply.github.com>
2023-11-14 17:37:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftWristX=0;
+rightWristX=0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -14,9 +17,16 @@ function setup() {
  
  function draw() {
     background('#a5b4d4')
+    text('Anushka', 80, 40);
+    textSize(difference);
+    console.log(textSize)
  }
  
- 
+ function textSize(){
+    fill('#93a8ed');
+
+ }
+
  function modelLoaded() {
     console.log(' PoseNet Is Initialized');
  }
@@ -27,6 +37,11 @@ function setup() {
     if(results.length > 0)
     {
         console.log(results);
+        leftWristX=results[0].pose.leftWrist.X;
+        rightWristX=results[0].pose.rightWrist.X;
+        console.log('leftWristX=' +leftWristX +'rightWristX=' +rightWristX);
+        difference= floor(leftWristX-rightWristX);
+        console.log('difference='+ difference)
     }
  }
  
